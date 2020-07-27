8ca42158aa1b480cc23a58b1a6dc05806748d8e3
Hexiang Zhang <sam10.zhang@gmail.com>
2020-07-27 17:53:36 -0500

    reorganize html, add direction key listener event

--- snake.js
+++ snake.js
@@ -14,32 +14,56 @@ foodImg.src = "img/food.png";
 // CREATE THE SNAKE
 let snake = [];
 snake[0] = {
-	x : 9 * box, 
-	y : 10 * box
+    x: 9 * box,
+    y: 10 * box
 }
 
 // CREATE THE FOOD
 let food = {
-	x : Math.floor(Math.random()*17 + 1) * box, 
-	y : Math.floor(Math.random()*15 + 3) * box
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
 }
 
 // CREATE THE SCORE VAR
 let score = 0;
 
+// CONTROL THE SNAKE
+let dir;
+
+document.addEventListener("keydown", direction);
+
+function direction(event) {
+    if (event.keyCode == 37) {
+        dir = "LEFT";
+    } else if (event.keyCode == 38) {
+        dir = "UP";
+    } else if (event.keyCode == 39) {
+        dir = "RIGHT";
+    } else if (event.keyCode == 40) {
+        dir = "DOWN";
+    }
+}
+
+
+
 // DRAW EVERYTHING TO THE CANVAS
 function draw() {
-	ctx.drawImage(ground, 0, 0);
-	
-	for(let i = 0; i < snake.length; i++) {
+    ctx.drawImage(ground, 0, 0);
 
-	}
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = (i == 0) ? "green" : "white";
+        ctx.fillStyle = (snake[i].x, snake[i].y, box, box);
 
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score, 2 * box, 1.6 * box);
 }
 
 // CALL DRAW FUNCTION EVERY 100 ms
 let game = setInterval(draw, 100);
-
-
-
-
